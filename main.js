becc26a0f68c7330717b7ef7053dca52cd93958b
Karla <105371331+stakarla@users.noreply.github.com>
2024-04-11 08:39:12 -0300

    ok

--- main.js
+++ main.js
@@ -16,6 +16,11 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date ("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date ("2023-12-05T00:00:00");
+const tempoObjetivo4 = new Date ("2023-12-05T00:00:00");
+
+const tempos = [tempoObjetivo!, tempoObjetivo2, tempoObjetiv3, tempoObjetivo4]:
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
